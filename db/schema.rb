2bc2bc9e2ec3d9930ca9e4c91a36a8655977b55b
daichi kouno <15214kouno@yuai.ac.jp>
2021-12-26 00:27:32 +0900

    foodテーブルの作成、バリデーションの導入、モデルの日本語設定を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_17_032703) do
+ActiveRecord::Schema.define(version: 2021_12_25_145541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "comment"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_12_17_032703) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "foods", "users"
 end
